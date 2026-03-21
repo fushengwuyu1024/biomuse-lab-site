eee5645857e4fcd9954bd85947907e36808f1845
BioMuse_lab <rightheretc@gmail.com>
2026-03-21 17:35:38 +0800

    Update config.ts

--- src/content/config.ts
+++ src/content/config.ts
@@ -8,6 +8,7 @@ const blog = defineCollection({
     date: z.coerce.date(),
     draft: z.boolean().optional(),
     drawer: z.string().optional(),
+    tags: z.array(z.string()).optional(),
   }),
 });
 
@@ -29,7 +30,7 @@ const projects = defineCollection({
     date: z.coerce.date(),
     draft: z.boolean().optional(),
     demoURL: z.string().optional(),
-    repoURL: z.string().optional()
+    repoURL: z.string().optional(),
   }),
 });
 
